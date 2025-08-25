355f09587bf9b4de5a57257cad7c9ddd8f380096
junehyeok0409-ui <junehyeok0409@gmail.com>
2025-08-25 16:54:08 +0900

    1

--- C언어/TEST2.5..cpp
+++ C언어/TEST2.5..cpp
@@ -8,6 +8,13 @@ int main()
 	printf("종료값을 입력하세요"); scanf("%d", &eval); 
 	
 	rval = 0;
+	//while문은 1.초기값 2.수행조건 3.증감 으로 이루어진다. 
+	//초기값은 최초 시작값을 정해주는 것
+	int index = sval;
+	while((index >= sval) && ( index <=eval)) //수행조건
+	{
+		
+	}
 	rval = rval + sval; sval++;
 	rval = rval + sval; sval++;
 	rval = rval + sval; sval++;
